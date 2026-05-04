0d34b966b2a1e175f4b668c8f4b0a487fa15bac0
miajose-sava <msantamariav@est.ups.edu.ec>
2026-05-03 21:53:45 -0500

    Subida inicial: Estructura de paquetes poo y uni1a

---
+++ uni1a/Investigador.java
@@ -0,0 +1,19 @@
+package uni1a;
+
+public class Investigador {
+	private String nombre;
+	
+	public Investigador(String nombre) {
+	this.setNombre(nombre);
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+
+}
